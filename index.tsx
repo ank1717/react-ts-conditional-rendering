f7ff23c2ab5fbc9b6cc99b2d351aa1bec488d606
ank1717 <ankurs1234@gmail.com>
2020-09-18 13:48:51 +0530

    Initial commit

---
+++ index.tsx
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import './style.css';
+
+// interface AppProps { }
+// interface AppState {
+//   name: string;
+// }
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {text: '', inputText: '', mode:'view'};
+    
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSave = this.handleSave.bind(this);
+    this.handleEdit = this.handleEdit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ inputText: e.target.value });
+  }
+  
+  handleSave() {
+    this.setState({text: this.state.inputText, mode: 'view'});
+  }
+
+  handleEdit() {
+    this.setState({mode: 'edit'});
+  }
+
+  render () {
+    const view = this.state.mode === 'view';
+    
+    return (
+      <div>
+        <p>Text: {this.state.text}</p>
+        
+        {
+          !view && (
+            <p>
+              <input
+                onChange={this.handleChange}
+                value={this.state.inputText} />
+            </p>
+          )
+        }
+        
+        {
+          (() => {
+            const handler = view 
+                ? this.handleEdit 
+                : this.handleSave;
+            const label = view ? 'Edit' : 'Save';
+          
+            return (
+              <button onClick={handler}>
+                {label}
+              </button>
+            );
+          })()
+        }  
+      </div>
+    );
+  }
+}
+
+render(
+  <App />,
+  document.getElementById('root')
+);
